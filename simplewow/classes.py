d5ee9d0723c763ceb7c128dbbc365d6c69cc9ff1
Scott Siegel <scottandsiegel@gmail.com>
2020-05-19 21:34:50 -0600

    classes works but is basic

--- simplewow/classes.py
+++ simplewow/classes.py
@@ -1,12 +1,12 @@
-import abilities
-
 class Hero(object):
     def __init__(self, name, lvl):
         self.name = name
         self.lvl = lvl
         self.hp = 100
         self.mana = 60
-        self.basic_dmg = 5
+        self.basic_dmg = 8
+        self.busy = False
+
     def basic_attack(self, target):
         target.hp -= self.basic_dmg
 
@@ -15,12 +15,13 @@ class Mage(Hero):
         super().__init__(name, lvl)
         self.hp -= 20
         self.mana += 20
-        self.basic_dmg -= 1
-        self.abilities = (self.basic_attack, self.fireblast)
-    def fireblast(self, target: Hero):
-        if self.mana >= 15:
-            print('kaboom!')
-            self.mana -= 15
+        self.basic_dmg -= 2
+        self.actions = (self.basic_attack, self.fireblast)
+
+    def fireblast(self, target):
+        if self.mana >= 20:
+            print('kapow!')
+            self.mana -= 20
             target.hp -= 30
         else:
             print('oom')
@@ -30,12 +31,12 @@ class Paladin(Hero):
         super().__init__(name, lvl)
         self.hp += 10
         self.mana -= 30
-        self.basic_dmg += 7
+        self.basic_dmg += 8
         self.abilities = (self.basic_attack, self.crusader_strike)
     def crusader_strike(self, target: Hero):
         if self.mana >= 20:
             print('kapow!')
             self.mana -= 20
-            target.hp -= 25
+            target.hp -= 20
         else:
             print('oom')
